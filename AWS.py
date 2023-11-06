c7df68b7c851fcbdce8a8b79455b6bc3f8e0af60
TaeHyun Park <pthkuku99@gmail.com>
2023-11-06 17:04:18 +0900

    key 읽는 방식 변경, 인스턴스 목록 출력

--- AWS.py
+++ AWS.py
@@ -1,10 +1,24 @@
 import boto3
-import os
+import csv
 import subprocess
+
+aws_acc_key = ''
+aws_scr_key = ''
 
 with open('rootkey.csv', 'r') as file:
     lines = file.readlines()
-    access_key = lines[0].strip()
-    secret_key = lines[1].strip()
+    keys = lines[1].strip().split(',')  # 두 번째 줄을 읽고 ','로 분리
 
+    if len(keys) == 2:
+        aws_acc_key, aws_scr_key = keys
 
+aws_region = 'ap-northeast-2'
+ec2 = boto3.client('ec2', aws_access_key_id=aws_acc_key, aws_secret_access_key=aws_scr_key, region_name=aws_region)
+
+# EC2 인스턴스 목록 가져오기
+response = ec2.describe_instances()
+
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        print(f"Instance ID: {instance['InstanceId']}")
+        print(f"Instance State: {instance['State']['Name']}")
